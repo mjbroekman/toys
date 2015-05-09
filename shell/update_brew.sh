b596b96701d2dd667070a6439967dec2d5328e6e
Maarten Broekman <mbroekman@maileig.com>
2015-05-09 11:22:22 -0400

    renamed and updated for a progress indicator

---
+++ shell/update_brew.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Author: Maarten Broekman
+#
+# Description:
+#   Code snippet to keep brew installs updated on MacOS X
+#
+if [ ! -f ~/.homebrew_updated ]; then
+    date +%Y%m%d >~/.homebrew_updated
+    echo "Checking for homebrew updates"
+    brew update >~/.homebrew_update_log 2>&1
+    BREW_CNT=`brew list | wc -l | awk '{ print $1 }'`
+    IDX=0
+    for f in `brew list`; do
+	IDX=$(( $IDX + 1 ))
+	printf "%s\r" "Performing homebrew update $IDX of $BREW_CNT"
+	brew upgrade $f >~/.homebrew_upgrade_log 2>&1
+    done
+    echo
+elif [ `date +%Y%m%d` -gt `cat ~/.homebrew_updated` ]; then
+    date +%Y%m%d >~/.homebrew_updated
+    cp ~/.homebrew_update_log ~/.homebrew_update_log.1
+    cp ~/.homebrew_upgrade_log ~/.homebrew_upgrade_log.1
+    echo "Checking for homebrew updates"
+    brew update >~/.homebrew_update_log 2>&1
+    BREW_CNT=`brew list | wc -l | awk '{ print $1 }'`
+    IDX=0
+    for f in `brew list`; do
+	IDX=$(( $IDX + 1 ))
+	printf "%s\r" "Performing homebrew update $IDX of $BREW_CNT"
+	brew upgrade $f >~/.homebrew_upgrade_log 2>&1
+    done
+    echo
+fi
