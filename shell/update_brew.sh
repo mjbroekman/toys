0cc92ea8dd8305f9adfb6996a2df6d3805d45a69
Maarten Broekman <maarten@broekman.us>
2015-12-06 18:13:19 -0500

    updated fortunes... updated brew script to try and not bork brew constantly

--- shell/update_brew.sh
+++ shell/update_brew.sh
@@ -11,29 +11,38 @@ elif [ -d ~/Dropbox ]; then
     brew list >~/Dropbox/brew_list.log
 fi
 if [ ! -f ~/.homebrew_updated ]; then
-    date +%Y%m%d >~/.homebrew_updated
     echo "Checking for homebrew updates"
     brew update >~/.homebrew_update_log 2>&1
-    BREW_CNT=`brew list | wc -l | awk '{ print $1 }'`
-    IDX=0
-    for f in `brew list`; do
-	IDX=$(( $IDX + 1 ))
-	printf "%s\r" "Performing homebrew update $IDX of $BREW_CNT"
-	brew upgrade $f >~/.homebrew_upgrade_log 2>&1
-    done
-    echo
+#    BREW_CNT=`brew list | wc -l | awk '{ print $1 }'`
+#    IDX=0
+#    for f in `brew list`; do
+#	IDX=$(( $IDX + 1 ))
+#	printf "%s\r" "Performing homebrew update $IDX of $BREW_CNT"
+#	brew upgrade $f >~/.homebrew_upgrade_log 2>&1
+#    done
+#    echo
 elif [ `date +%Y%m%d` -gt `cat ~/.homebrew_updated` ]; then
-    date +%Y%m%d >~/.homebrew_updated
-    cp ~/.homebrew_update_log ~/.homebrew_update_log.1
-    cp ~/.homebrew_upgrade_log ~/.homebrew_upgrade_log.1
+    if [ -f ~/.homebrew_update_log ]; then
+	mv ~/.homebrew_update_log ~/.homebrew_update_log.1
+    fi
+    if [ -f ~/.homebrew_upgrade_log ]; then
+	mv ~/.homebrew_upgrade_log ~/.homebrew_upgrade_log.1
+    fi
     echo "Checking for homebrew updates"
     brew update >~/.homebrew_update_log 2>&1
-    BREW_CNT=`brew list | wc -l | awk '{ print $1 }'`
-    IDX=0
-    for f in `brew list`; do
-	IDX=$(( $IDX + 1 ))
-	printf "%s\r" "Performing homebrew update $IDX of $BREW_CNT"
-	brew upgrade $f >~/.homebrew_upgrade_log 2>&1
-    done
-    echo
+#    BREW_CNT=`brew list | wc -l | awk '{ print $1 }'`
+#    IDX=0
+#    for f in `brew list`; do
+#	IDX=$(( $IDX + 1 ))
+#	printf "%s\r" "Performing homebrew update $IDX of $BREW_CNT"
+#	brew upgrade $f >~/.homebrew_upgrade_log 2>&1
+#    done
+#    echo
+fi
+
+if [ `date +%w` -eq 0 -a `date +%Y%m%d` -gt `cat ~/.homebrew_updated` ]; then
+    date +%Y%m%d >~/.homebrew_updated
+    BLIST=`brew list`
+    brew uninstall --force $BLIST >~/.homebrew_upgrade_log 2>&1 
+    brew install $BLIST >~/.homebrew_upgrade_log 2>&1 
 fi
