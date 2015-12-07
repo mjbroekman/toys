28f24d72a375ece7db9801f3af843c8ba74b0e17
Maarten Broekman <maarten@broekman.us>
2015-12-07 10:12:03 -0500

    update logic to speed up logins

--- shell/update_brew.sh
+++ shell/update_brew.sh
@@ -7,12 +7,10 @@
 #
 if [ ! -x /usr/local/bin/brew ]; then
     exit 0
-elif [ -d ~/Dropbox ]; then
-    brew list >~/Dropbox/brew_list.log
 fi
 if [ ! -f ~/.homebrew_updated ]; then
-    echo "Checking for homebrew updates"
-    brew update >~/.homebrew_update_log 2>&1
+  echo "Checking for homebrew updates"
+  brew update >~/.homebrew_update_log 2>&1
 #    BREW_CNT=`brew list | wc -l | awk '{ print $1 }'`
 #    IDX=0
 #    for f in `brew list`; do
@@ -22,21 +20,24 @@ if [ ! -f ~/.homebrew_updated ]; then
 #    done
 #    echo
 elif [ `date +%Y%m%d` -gt `cat ~/.homebrew_updated` ]; then
-    date +%Y%m%d >~/.homebrew_updated
-    if [ -f ~/.homebrew_update_log ]; then
-	mv ~/.homebrew_update_log ~/.homebrew_update_log.1
-    fi
-    if [ -f ~/.homebrew_upgrade_log ]; then
-	mv ~/.homebrew_upgrade_log ~/.homebrew_upgrade_log.1
-    fi
-    echo "Checking for homebrew updates"
-    brew update >~/.homebrew_update_log 2>&1
+  if [ -d ~/Dropbox ]; then
+    brew list >~/Dropbox/brew_list.log
+  fi
+  date +%Y%m%d >~/.homebrew_updated
+  if [ -f ~/.homebrew_update_log ]; then
+    mv ~/.homebrew_update_log ~/.homebrew_update_log.1
+  fi
+  if [ -f ~/.homebrew_upgrade_log ]; then
+    mv ~/.homebrew_upgrade_log ~/.homebrew_upgrade_log.1
+  fi
+  echo "Checking for homebrew updates"
+  brew update >~/.homebrew_update_log 2>&1
 
-    if [ `date +%w` -eq 0 ]; then
-	BLIST=`brew list`
-	brew uninstall --force $BLIST >~/.homebrew_upgrade_log 2>&1 
-	brew install $BLIST >~/.homebrew_upgrade_log 2>&1 
-    fi
+  if [ `date +%w` -eq 0 ]; then
+    BLIST=`brew list`
+    brew uninstall --force $BLIST >~/.homebrew_upgrade_log 2>&1
+    brew install $BLIST >~/.homebrew_upgrade_log 2>&1
+  fi
 #    BREW_CNT=`brew list | wc -l | awk '{ print $1 }'`
 #    IDX=0
 #    for f in `brew list`; do
