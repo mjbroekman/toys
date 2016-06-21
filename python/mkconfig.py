fd875560e2f52d8f2da6b0919479736e616f70f0
Maarten Broekman <maarten.broekman@gmail.com>
2016-06-21 13:23:31 -0400

    Changed options and functionality to be better suited for variable insertion/removal and hash variables

--- python/mkconfig.py
+++ python/mkconfig.py
@@ -12,7 +12,10 @@ import os
 import string
 import socket
 
-TYPES = ['host', 'service', 'svc', 'notification', 'contact', 'alert']
+CONF_DIR = "/etc/icinga2/repository.d/hosts"
+ADD_HOST = "/usr/sbin/icinga2 repository host add"
+ADD_SVC = "/usr/sbin/icinga2 repository service add"
+COMMIT = "/usr/sbin/icinga2 repository commit"
 
 def usage(msg):
     """
@@ -20,21 +23,28 @@ def usage(msg):
     """
     print(msg)
     print('mkconfig.py [options]')
-    print(' -T : config type', TYPES)
     print(' -H : hostname')
-    print(' -C : check command')
-    print(' -a : ip address')
-    print(' -c : critical level')
-    print(' -e : email address')
-    print(' -g : group')
-    print(' -p : port')
-    print(' -s : object name')
-    print(' -u : user name')
-    print(' -v : vhost name')
-    print(' -w : warning level')
+    print(' -S : service to modify')
+    print(' -V : variable setting to change')
     print('')
-    print(' -j : JSON string containing configuration information')
-    print(' -J : File containing JSON data of configuration information')
+    print(' Format for -V:')
+    print('   Scalar variables -> variable_name:value')
+    print('   Hash variables   -> variable_name["key"]:{subvariable:value^subvariable:value^...}')
+    print('')
+    print(' If -S is not used, variables will be modified within the host object')
+    print(' If variable_name is not set, it will be added.')
+    print(' If variable_name is already set, it will be modified')
+    print(' If value is empty, variable_name will be set to an empty string')
+    print(' To remove a variable, prepend the variable_name with -')
+    print('')
+    print('Example 1: Simple override of the SSH check to use a different command')
+    print(' mkconfig.py -H nismaster.iscinternal.com -S ssh -V', end='')
+    print(' check_command:tcp -V vars.tcp_port:22')
+    print('')
+    print('Example 2: More complex setting of HTTP checks on the host')
+    print(' mkconfig.py -H login-us.mimecast.com', end='')
+    print(' -V vars.http_vhosts["login"]:{http_vhost:login-us.mimecast.com^http_uri:/logon}')
+    print('')
 
 
 def init_config():
@@ -43,31 +53,11 @@ def init_config():
     """
 
     cfg = {}
-    cfg['type'] = None          # -T
-    cfg['hname'] = None         # -H
-    cfg['check'] = None         # -C
-    cfg['address'] = None       # -a
-    cfg['crit'] = -1            # -c
-    cfg['email'] = None         # -e
-    cfg['group_name'] = None    # -g
-    cfg['port'] = 0             # -p
-    cfg['oname'] = None         # -s
-    cfg['user_name'] = None     # -u
-    cfg['vhost'] = None         # -v
-    cfg['warn'] = -1            # -w
+    cfg['host'] = None          # -H
+    cfg['svc'] = None           # -S
+    cfg['vars'] = []            # -V
 
     return cfg
-
-
-def test_type(objtype):
-    """
-    Check that the provided type is valid.
-    """
-    if objtype in TYPES:
-        return objtype
-    else:
-        usage("Unknown configuration type: " + objtype)
-        sys.exit(2)
 
 
 def test_addr(addr):
@@ -103,12 +93,66 @@ def test_email(email):
     return email
 
 
+def create_host(host_name):
+    """
+    Create a new host object
+    """
+    cmd = ADD_HOST + " name=" + host_name + " address=" + host_name + " check_command=hostalive"
+    print('Do you wish to create a new host object? (y/N) ', end='')
+    ans = sys.stdin.read(1)
+    if ans != "y" and ans != "Y":
+        print('Create the host first using:')
+        print(cmd)
+        sys.exit(1)
+
+    try:
+        os.system(cmd)
+    except OSError as err:
+        print('An error occurred while trying to run:\n' + cmd + '\n' + COMMIT)
+        print(err)
+        sys.exit(2)
+    except Warning:
+        print('Warning occurred: ' + sys.exc_info()[0])
+    else:
+        os.system(COMMIT)
+
+
+def check_config(config):
+    """
+    Validate that all the config settings are 'okay'
+    """
+    host_conf = CONF_DIR + "/" + config['host'] + ".conf"
+
+    try:
+        host_file = open(host_conf, 'r')
+    except IOError:
+        first_dot = config['host'].find('.')
+        short_host = config['host'][:first_dot]
+        short_conf = CONF_DIR + "/" + short_host + ".conf"
+        print('Short hostname = ' + short_host)
+        try:
+            host_file = open(short_conf, 'r')
+        except IOError:
+            print('No such host file (' + host_conf + ' or ' + short_conf + ') found.')
+            print('')
+            create_host(config['host'])
+            # sys.exit(3)
+        else:
+            host_file.close()
+            config['host'] = short_host
+            host_conf = short_conf
+    else:
+        host_file.close()
+
+    # host_file exists, so we know the host's service directory _might_ exist
+
+
 def main(args):
     """
     Process options and figure out what we're creating
     """
     try:
-        opts, args = getopt.getopt(args, "T:H:C:a:v:w:c:p:e:u:g:s:j:J:")
+        opts, args = getopt.getopt(args, "H:S:V:")
     except getopt.GetoptError:
         usage("Unknown option")
         sys.exit(2)
@@ -116,19 +160,15 @@ def main(args):
     config = init_config()
 
     for opt, arg in opts:
-        if opt == "-T":
-            config['type'] = test_type(arg)
-        elif opt == "-H":
-            config['hname'] = arg
-        elif opt == "-C":
-            config['check'] = arg
-        elif opt == "-a":
-            config['address'] = test_addr(arg)
-        elif opt == "-c":
-            config['check'] = arg
-        elif opt == "-e":
-            config['email'] = test_email(arg)
+        if opt == "-H":
+            config['host'] = arg
+        elif opt == "-S":
+            config['svc'] = arg
+        elif opt == "-V":
+            config['vars'].append(arg)
 
+    print(config)
+    check_config(config)
 
 if __name__ == '__main__':
     try:
