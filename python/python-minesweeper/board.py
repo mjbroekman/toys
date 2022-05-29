4dddebe184a2a42fa8880946481aff4d1178f614
Maarten Broekman <maarten@broekman.us>
2022-05-28 22:52:35 -0400

    mjbroekman - set up number of mines remaining and fixed board size limits

--- python/python-minesweeper/board.py
+++ python/python-minesweeper/board.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 from cell import GameCell
 
 class GameBoard:
@@ -16,6 +18,17 @@ class GameBoard:
             y_size (int): Vertical size
             num_mines (int, optional): Number of mines. Defaults to -1 (random).
         """
+        self._get_term_size()
+        self.x_size = x_size
+        self.y_size = y_size
+        self.mines_left = num_mines
+
+    def _get_term_size(self):
+        """(private) Get the size of the terminal for boundary checking
+
+        Raises:
+            ValueError: if the screen is too narrow or too short
+        """
         if os.get_terminal_size()[0] > 36:
             self._max_x = 26
         else:
@@ -32,8 +45,20 @@ class GameBoard:
         if self._max_y < 0:
             raise ValueError("Screen is too short. Minimum screen height is 7 rows.")
 
-        self.x_size = x_size
-        self.y_size = y_size
+
+    @property
+    def mines_left(self) -> int:
+        return self._mines_left
+    
+    @mines_left.setter
+    def mines_left(self, mines: int):
+        if mines < 1:
+            mines = random.randrange(1, (self.x_size * self.y_size))
+
+        if mines > ((self.x_size * self.y_size) - 1):
+            raise ValueError("Too many mines to fit. Must be between 1 and " + str((self.x_size * self.y_size) - 1))
+
+        self._mines_left = mines
 
     @property
     def x_size(self) -> int:
@@ -41,7 +66,7 @@ class GameBoard:
     
     @x_size.setter
     def x_size(self, size: int):
-        if 1 < size < self._max_x:
+        if 1 < size <= self._max_x:
             self._x_size = size
         else:
             raise ValueError("Board width must be between 2 and " + str(self._max_x) + " cells.")
@@ -52,8 +77,10 @@ class GameBoard:
     
     @y_size.setter
     def y_size(self, size: int):
-        if 1 < size < self._max_y:
+        if 1 < size <= self._max_y:
             self._y_size = size
         else:
             raise ValueError("Board height must be between 2 and " + str(self._max_y) + " cells")
 
+    def create_board(self):
+        pass
