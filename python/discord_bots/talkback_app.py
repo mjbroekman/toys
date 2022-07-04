c34b0fd414143d6642b61587f41eb91b5fff8455
Maarten Broekman <maarten@broekman.us>
2022-07-04 14:01:52 -0400

    mjbroekman - added ability to save, delete, and list local quotes

--- python/discord_bots/talkback_app.py
+++ python/discord_bots/talkback_app.py
@@ -6,6 +6,7 @@ import random
 import requests
 import json
 import emoji
+from replit import db
 
 
 client = discord.Client()
@@ -14,13 +15,14 @@ client = discord.Client()
 def send_command(command):
     commands = [
         '$help', '$hello', '$hi', '$howdy', '$sup', '$commands', '$youtube',
-        '$github', '$replit', '$random', '$socials', '$quote'
+        '$github', '$replit', '$random', '$socials', '$quote', '$quotes',
+        '$addquote', '$delquote', '$listquotes'
     ]
 
     if command in ['$help', '$commands']:
         return 'These are the commands I understand:\n' + ', '.join(commands) +
-               '\n\nI also respond to questions or if you mention me.\n\n' +
-               'And if you say "pass the ", I will try to give it to you or ' + 
+               '\n\nI also respond to questions or if you mention me.\n\n' + 
+               'If you say "pass the ", I will try to give it to you or ' +
                'someone else. If possible, I will even turn it into emoji.'
 
     if command in ['$hello', '$hi', '$howdy', '$sup']:
@@ -40,33 +42,47 @@ def send_command(command):
 
     if command in ['$quote']:
         return get_quote()
-  
+
+    if command.startswith('$addquote'):
+        return add_quote(command)
+
+    if command.startswith('$delquote'):
+        return del_quote(command)
+
+    if command in ['$quotes', '$listquotes']:
+      if 'quotes' in db.keys():
+          if len(db['quotes']) > 0:
+              return "Current stored quotes:\n* " + "\n* ".join(db['quotes'])
+      return "There are no quotes stored in the bot yet. Using the $quote command will get quotes from https://quotable.io. Using $addquote will add quotes to the bot."
+
     if command.startswith('$random'):
         if command == '$random':
-            return "Here's a random number: " + str(random.randint(0, int(time.time())))
+            return "Here's a random number: " + str(
+                random.randint(0, int(time.time())))
 
         num1 = re.match(r'.random (-?\d+)$', command)
         nums = re.match(r'.random (-?\d+) (-?\d+)', command)
         if num1 is not None:
             if int(num1.group(1)) > 0:
-                return "Here's a random number between 0 and " + num1.group(1) +
-                       ":\n\t" + str(random.randint(0, int(num1.group(1))))
+                return "Here's a random number between 0 and " + num1.group(
+                    1) + ":\n\t" + str(random.randint(0, int(num1.group(1))))
             if int(num1.group(1)) < 0:
-                return "Here's a random number between " + num1.group(1) +
-                       "and 0:\n\t" + str(random.randint(int(num1.group(1)), 0))
+                return "Here's a random number between " + num1.group(
+                    1) + "and 0:\n\t" + str(
+                        random.randint(int(num1.group(1)), 0))
 
             return "That's not very random, don't you think?"
 
         if nums is not None:
             if int(nums.group(1)) < int(nums.group(2)):
-                return "Here's a random number between " + nums.group(1) +
-                       " and " + nums.group(2) + ":\n\t" + 
-                       str(random.randint(int(nums.group(1)), int(nums.group(2))))
+                return "Here's a random number between " + nums.group(
+                    1) + " and " + nums.group(2) + ":\n\t" + str(
+                        random.randint(int(nums.group(1)), int(nums.group(2))))
 
             if int(nums.group(1)) > int(nums.group(2)):
-                return "Here's a random number between " + nums.group(2) +
-                       " and " + nums.group(1) + ":\n\t" +
-                       str(random.randint(int(nums.group(2)), int(nums.group(1))))
+                return "Here's a random number between " + nums.group(
+                    2) + " and " + nums.group(1) + ":\n\t" + str(
+                        random.randint(int(nums.group(2)), int(nums.group(1))))
 
             return "That's not very random, don't you think?"
 
@@ -75,14 +91,46 @@ def send_command(command):
     return "What was that? I didn't understand that command:\n\t" + command + "\n\nTry $help"
 
 
-def get_quote():
-  try:
-    response = requests.get("https://quotable.io/random")
-    quote_data = json.loads(response.text)
-    return quote_data['content'] + ' -' + quote_data['author']
-  except Exception as e:
-    return "There was a problem getting quotes from https://quotable.io/random" + str(e)
+def add_quote(command):
+  quote_text = command.replace('$addquote ','')
+  if quote_text.startswith('-'):
+    return "Don't start your quote with a -. I will automatically add the attribution."
 
+  if 'quotes' not in db.keys():
+    db['quotes'] = []
+
+  if quote_text in db['quotes']:
+    return "That quote is already stored locally"
+
+  db['quotes'].append(quote_text)
+  return "Added quote to local storage."
+
+
+def del_quote(command):
+  quote_text = command.replace('$delquote ','')
+
+  if 'quotes' not in db.keys():
+    return "No quotes stored locally"
+
+  if quote_text not in db['quotes']:
+    return "Unable to find quote to delete. Did you type it correctly?"
+  
+  db['quotes'].remove(quote_text)
+  return "Removed quote: '" + quote_text + "'"
+
+  
+def get_quote():
+  if random.randint(0,1) == 0 or 'quotes' not in db.keys() or len(db['quotes']) < 1:
+    try:
+      response = requests.get("https://quotable.io/random")
+      quote_data = json.loads(response.text)
+      return quote_data['content'] + ' -' + quote_data['author']
+    except Exception as e:
+      return "There was a problem getting quotes from https://quotable.io/random" + str(e)
+  else:
+    if 'quotes' in db.keys():
+      return db['quotes'][random.randint(0,(len(db['quotes']) - 1))]
+      
 
 def check_words(word_string,target):
   try:
@@ -97,7 +145,6 @@ def check_words(word_string,target):
         else:
           new_string = new_string + " " + word
       return new_string + " to " + target
-
   except Exception as e:
     return 'Ran into an error: ' + str(e)
 
@@ -114,8 +161,12 @@ async def on_message(message):
             return
 
         if message.content.startswith('$'):
+            command = message.content
             print('Received a command: ' + message.content)
-            await message.channel.send(send_command(message.content))
+            if command.startswith('$addquote'):
+              command = command + " -" + message.author.display_name
+
+            await message.channel.send(send_command(command))
             return
 
         if "pass the " in message.content.lower():
@@ -158,10 +209,8 @@ async def on_message(message):
         await message.channel.send('Take a look at THAT:\n' + str(e))
         return
 
-
 try:
   client.run(os.environ['CLIENT_TOKEN'])
 except Exception as e:
   print('There is a problem running the client. Did you make sure to set the CLIENT_TOKEN environment variable?')
   print(e)
-  
